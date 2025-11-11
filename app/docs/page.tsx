7de8e9cebec0c37d86562ac17fab816d8a8ef06b
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-11-10 22:08:46 -0600

    create documentation page

---
+++ app/docs/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function DeveloperDocumentation() {
+    return (
+        <div id="developer-documentation-page">
+            <h1>Developer Documentation</h1>
+            <p>Your guide to developing orbt widgets</p>
+        </div>
+    );
+}
