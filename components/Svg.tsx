71e04844b6df26bdbb61f1209b96473ae939c4c8
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-11-10 22:06:23 -0600

    fix svg component

--- components/Svg.tsx
+++ components/Svg.tsx
@@ -11,18 +11,10 @@ export default function Svg({ name, className }: { name: string; className?: str
 
     if (!icon) return null;
 
-    const iconProps = Object.assign(
-        icon.split("'").flatMap((part, index, arr) => {
-            // ignore if part starts with / or > using regex
-            if (index % 2 === 0) return [];
-            return [`${rmSymb(arr[index - 1])}${rmSymb(part)}`];
-        }).slice(1).reduce((acc, curr) => {
-            let [key, value] = curr.split('=');
-            key = key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
-            acc[key] = value.replace(/"/g, '');
-            return acc;
-        }, {} as { [key: string]: string })
+    return (
+        <span
+            className={`icon ${className}`}
+            dangerouslySetInnerHTML={{ __html: icon }}
+        />
     );
-
-    return <svg {...iconProps}></svg>;
 }
