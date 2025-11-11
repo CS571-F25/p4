6ed17ec26d6ecc942c02428db24dce40cb8b76ce
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-11-10 22:08:21 -0600

    create connections page

---
+++ app/connections/page.tsx
@@ -0,0 +1,17 @@
+import '@/styles/providers.css';
+
+import providers from '@/data/providers.json';
+import ProviderButton from '@/components/main/ProviderButton';
+
+export default function Connections() {
+    return (
+        <div id="provider-container">
+            <p>include events & data from popular services</p>
+            <div id="provider-button-container">
+                {Object.entries(providers).map(([provider, { color }], i) => (
+                    <ProviderButton key={i} provider={provider} color={color} />
+                ))}
+            </div>
+        </div>
+    );
+}
