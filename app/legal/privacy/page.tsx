64b814dcde1abe5912dce7d73650fa022a6e86a8
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-11-10 22:13:16 -0600

    create legal pages

---
+++ app/legal/privacy/page.tsx
@@ -0,0 +1,7 @@
+export default function PrivacyPolicy() {
+    return (
+        <div id="privacy-policy-page">
+            <h1>Privacy Policy</h1>
+        </div>
+    );
+}
---
+++ app/legal/terms/page.tsx
@@ -0,0 +1,7 @@
+export default function Terms() {
+    return (
+        <div id="terms-page">
+            <h1>Terms of Service</h1>
+        </div>
+    );
+}
