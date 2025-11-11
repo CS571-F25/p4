1a850bd97e7909bae3dc4dd586d1385aa37d3ef3
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-11-10 22:08:37 -0600

    update content for main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
 import '@/styles/index.css';
 
-import Svg from '@/components/Svg';
 import AnimatedText from '@/components/AnimatedText';
+import Feature from '@/components/index/Feature';
 
-export default function MainPage() {
+const features = [
+    {
+        title: 'multi-platform support',
+        description: 'orbt widgets support events from multiple platforms like Twitch, YouTube, and more.',
+        imgSrc: 'https://i.etsystatic.com/17233766/r/il/a8c546/6959385544/il_1588xN.6959385544_p8sv.jpg',
+    },
+    {
+        title: 'consistent settings',
+        description: 'orbt widgets allow for consistent themes and goals that can easily be updated across all widgets.',
+        imgSrc: 'https://i.etsystatic.com/17233766/r/il/a8c546/6959385544/il_1588xN.6959385544_p8sv.jpg',
+    }
+];
+
+export default function Orbt() {
     return (
-        <div id="star-box" className="min-h-screen">
-            <h1 id="logo">orbt</h1>
+        <div id="orbt-page">
+            <span id="orbt-header">
+                <AnimatedText text="unique, versatile, simple." className="orbt-subheader" />
+                <h2>flexible content widgets that do more.</h2>
+            </span>
+            <div id="feature-box">
+                {features.map((feature, i) => (
+                    <Feature key={i} {...feature} i={i} />
+                ))}
+            </div>
         </div>
     );
 }
