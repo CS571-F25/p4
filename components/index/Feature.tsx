c309f5222e75721563ca7f5f09c573747b5f7d5f
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-11-10 22:12:16 -0600

    create component for displaying features on the main page

---
+++ components/index/Feature.tsx
@@ -0,0 +1,27 @@
+export default function Feature({
+    i,
+    title,
+    description,
+    imgSrc,
+}: {
+    i: number;
+    title: string;
+    description: string;
+    imgSrc: string;
+}) {
+    return (
+        <div className="feature" style={{ '--feature-index': i } as React.CSSProperties}>
+            <h3>{title}</h3>
+            <div className="feature-card">
+                <div className="feature-card-inner">
+                    <div className="feature-card-front shadow card">
+                        <img src={imgSrc} alt={title} />
+                    </div>
+                    <div className="feature-card-back shadow card">
+                        <p>{description}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
