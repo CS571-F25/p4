d1b29f6689c30f88162b27ee53d114b3d9b84be7
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-11-11 20:05:39 -0600

    improve provider check functionality

--- components/main/ProviderButtonCheck.tsx
+++ components/main/ProviderButtonCheck.tsx
@@ -3,61 +3,38 @@ import { auth } from '@/auth';
 
 import Svg from '@/components/Svg';
 
-export default function ClientTest({ provider, onClick }: { provider?: string, onClick?: () => void }) {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const providerId = '';
+export default function ClientTest({ provider }: { provider?: string }) {
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
 
     useEffect(() => {
         async function fetchProviders() {
             try {
-                if (provider && providerId) {
-                    await fetch(`/api/user/cookie`, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({
-                            provider,
-                            providerId,
-                        }),
-                    });
-                }
-
                 const response = await fetch(`/api/user/`);
                 if (!response.ok) {
                     throw new Error('Failed to fetch providers');
                 }
                 const data = await response.json();
-                setIsLoggedIn(data.includes(provider));
+                setIsLoggedIn(!!provider && data[provider] !== undefined);
+                console.log('Fetched providers:', data);
             } catch (error) {}
         }
 
         fetchProviders();
     }, []);
 
-    let background = (
-        <div className="provider-badge-inactive"></div>
-    )
-    if (isLoggedIn) {
-        background = (
-            <Svg name="badge" className="provider-badge" />
-        )
+    if (isLoggedIn === null) {
+        return <div className="provider-badge-loading"></div>;
     }
 
-    let content = (
+    let background = <div className="provider-badge-inactive"></div>;
+    if (isLoggedIn) {
+        background = <Svg name="badge" className="provider-badge" />;
+    }
+
+    return (
         <>
             {background}
             <Svg name={provider || ''} className="provider-icon" />
         </>
     );
-
-    if (onClick) {
-        return (
-            <button className="provider-button" onClick={onClick}>
-                {content}
-            </button>
-        );
-    }
-
-    return content;
 }
