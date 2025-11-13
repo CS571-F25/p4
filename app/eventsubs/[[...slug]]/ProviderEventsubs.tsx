a124a1785594c333001a414d20f2a5d44a05a5c3
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-11-13 16:01:22 -0600

    mobilize eventsub provider page

--- app/eventsubs/[[...slug]]/ProviderEventsubs.tsx
+++ app/eventsubs/[[...slug]]/ProviderEventsubs.tsx
@@ -3,14 +3,17 @@ import '@/styles/eventsubsProvider.css';
 
 import providers from '@/data/providers.json';
 
-import TextBubble from '@/components/TextBubble';
-import EventButton from '@/components/eventsubs/EventButton';
 import SVG from '@/components/Svg';
 import Clipboard from '@/components/Clipboard';
 import OrbtId from '@/components/OrbtId';
 
+import TextBubble from '@/components/TextBubble';
+import EventButton from '@/components/eventsubs/EventButton';
+import ActivityLog from '@/components/eventsubs/ActivityLog';
+
 export default function ProviderEventsubs({ provider }: { provider: keyof typeof providers }) {
     const [widgetPreview, setWidgetPreview] = useState('');
+    const [openModal, setOpenModal] = useState('');
 
     function pastePreview() {
         navigator.clipboard.readText().then((text) => {
@@ -18,10 +21,33 @@ export default function ProviderEventsubs({ provider }: { provider: keyof typeof
         });
     }
 
+    function changeModal(modal: string) {
+        if (openModal === modal) setOpenModal('');
+        else setOpenModal(modal);
+    }
+
     return (
         <div id="provider-eventsubs">
+            <span className="mobile-only mobile-buttons">
+                <button onClick={() => changeModal('test-events')}>
+                    <SVG name="gear-1" tooltip={{ text: 'test events', location: 'right' }} />
+                </button>
+                <button onClick={() => changeModal('goals')}>
+                    <SVG name="medal-2" tooltip={{ text: 'goals', location: 'right' }} />
+                </button>
+                <button onClick={() => changeModal('activity-log')}>
+                    <SVG name="message-2" tooltip={{ text: 'activity log', location: 'right' }} />
+                </button>
+                <button onClick={() => changeModal('orbt-id')}>
+                    <SVG name="star" tooltip={{ text: 'view orbtId', location: 'right' }} />
+                </button>
+            </span>
             <div id="goals-test-events" className="eventsubs-provider-container">
-                <div id="test-events" className="eventsubs-provider-box">
+                <div
+                    id="test-events"
+                    className={`eventsubs-provider-box modal ${openModal === 'test-events' ? 'open' : ''}`}
+                    onClick={() => changeModal('')}
+                >
                     <TextBubble>{provider}: test events</TextBubble>
                     <div id="test-event-buttons">
                         {Object.entries(providers?.[provider]?.eventsub)?.map(([name, event], i) => (
@@ -34,16 +60,23 @@ export default function ProviderEventsubs({ provider }: { provider: keyof typeof
                                         .join(' ')
                                         .toLowerCase() || name
                                 }
+                                event={name}
                             />
                         ))}
                     </div>
                 </div>
-                <div id="goals" className="eventsubs-provider-box">
-                    <h2>manage Goals</h2>
-                    <p>goals apply to all widgets</p>
+                <div
+                    id="goals"
+                    className={`eventsubs-provider-box modal ${openModal === 'goals' ? 'open' : ''}`}
+                    onClick={() => changeModal('')}
+                >
+                    <span>
+                        <h2>manage goals</h2>
+                        <p>goals apply to all widgets</p>
+                    </span>
                 </div>
             </div>
-            <div id="widget-preview" className="eventsubs-provider-box">
+            <div id="widget-preview" className="eventsubs-provider-box" onClick={() => changeModal('')}>
                 <div id="widget-preview-input-box">
                     <input
                         id="widget-preview-input"
@@ -54,15 +87,20 @@ export default function ProviderEventsubs({ provider }: { provider: keyof typeof
                         disabled
                     />
                     <Clipboard onClick={pastePreview} />
-                    <button onClick={() => setWidgetPreview('')}>
-                        <SVG name="mark-x" />
+                    <button className="flex justify-center items-center" onClick={() => setWidgetPreview('')}>
+                        <SVG name="mark-x" tooltip={{ text: 'Clear' }} />
                     </button>
                 </div>
                 {widgetPreview && <iframe src={widgetPreview} title="Widget Preview" />}
-                <OrbtId color="light" compact={true} />
+                <div
+                    id="orbt-id-modal"
+                    className={`flex align-center !justify-center modal ${openModal === 'orbt-id' ? 'open eventsubs-provider-box ' : ''}`}
+                >
+                    <OrbtId color="light" compact={true} />
+                </div>
             </div>
-            <div id="activity" className="eventsubs-provider-box">
-                <TextBubble>activity log</TextBubble>
+            <div id="activity" className={`eventsubs-provider-box modal ${openModal === 'activity-log' ? 'open' : ''}`}>
+                <ActivityLog provider={provider} />
             </div>
         </div>
     );
