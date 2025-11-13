73fe1290abd3a918709f3b286b0eb1b5861d9e62
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-11-13 16:02:12 -0600

    add tooltip component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,13 +12,13 @@ export default function Footer() {
                 </span>
                 <span id="footer-links" className="footer-block">
                     <a href="/discord">
-                        <SVG name="discord" />
+                        <SVG name="discord" tooltip={{ text: 'support discord' }} />
                     </a>
                     <a href="/shop">
-                        <SVG name="shop" />
+                        <SVG name="shop" tooltip={{ text: 'shop orbt widgets' }} />
                     </a>
                     <a href="/docs">
-                        <SVG name="book" />
+                        <SVG name="book" tooltip={{ text: 'developer docs' }} />
                     </a>
                     <a href="/legal/terms">terms</a>
                     <a href="/legal/privacy">privacy policy</a>
