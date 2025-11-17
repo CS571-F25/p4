da3745d81bfc6a8cae73f453a29f5ab4e48d4722
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-11-17 10:27:39 -0600

    clean up text bubble component

--- components/TextBubble.tsx
+++ components/TextBubble.tsx
@@ -1,9 +1,4 @@
-
 // will wrap text (not in an element)
 export default function TextBubble({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="text-bubble">
-            <p>{children}</p>
-        </div>
-    );
+    return <div className="text-bubble">{children}</div>;
 }
