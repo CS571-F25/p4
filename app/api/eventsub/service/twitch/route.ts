ae6282278f5b37277a5b98a0ac6c8e7bd4d78769
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-11-18 09:32:51 -0600

    implement GET endpoint to retrieve Twitch EventSub subscriptions for a user

--- app/api/eventsub/service/twitch/route.ts
+++ app/api/eventsub/service/twitch/route.ts
@@ -42,3 +42,32 @@ export async function POST(req: NextRequest) {
     // For other message types or errors
     return new NextResponse('Bad Request', { status: 400 });
 }
+
+// GET endpoint to retrieve Twitch EventSub subscriptions for a user
+export async function GET(req: Request) {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get('userId');
+    if (!userId) {
+        return NextResponse.json({ error: 'Twitch userId is required' }, { status: 401 });
+    }
+
+    const accessToken = await fetch(`${process.env.API}/eventsub/service/twitch/appaccess`).then((res) =>
+        res.json().then((data) => data.accessToken)
+    );
+
+    const response = await fetch('https://api.twitch.tv/helix/eventsub/subscriptions', {
+        method: 'GET',
+        headers: {
+            'Client-Id': process.env.AUTH_TWITCH_ID || '',
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data.data[0]), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
