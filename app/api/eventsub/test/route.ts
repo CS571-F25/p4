a426a013d6404f0ce1adab513c2d5d16924809de
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-11-17 21:08:12 -0600

    add data parameter to POST request and create a GET endpoint to receive an example event

--- app/api/eventsub/test/route.ts
+++ app/api/eventsub/test/route.ts
@@ -6,7 +6,7 @@ import providers from '@/data/providers.json';
 import { generateWithFormat } from '../utils/generateData';
 
 export async function POST(req: NextRequest) {
-    const { service, subscriptionType, userId } = await req.json();
+    const { service, subscriptionType, userId, data } = await req.json();
     const typedService = service as keyof typeof providers;
     const typedSubscriptionType = subscriptionType as keyof (typeof providers)[typeof typedService]['eventsub'];
 
@@ -28,11 +28,47 @@ export async function POST(req: NextRequest) {
         formatter = undefined;
     }
 
-    const mockData = generateWithFormat(service, formatter);
+    const mockData = data ?? generateWithFormat(service, formatter);
 
     await sendEventToUser(userId, { service, subscription: subscriptionType, mock: true, data: mockData }).catch((err) => {
         return new NextResponse(`Failed to send event: ${err.message}`, { status: 500 });
     });
 
     return new NextResponse('Event sent', { status: 200 });
+}
+
+// GET endpoint returns an example event for a given service and subscriptionType
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url);
+    const service = searchParams.get('service');
+    const subscriptionType = searchParams.get('subscriptionType');
+
+    if (!service || !subscriptionType) {
+        return new NextResponse('Missing required fields', { status: 400 });
+    }
+
+    const typedService = service as keyof typeof providers;
+    const typedSubscriptionType = subscriptionType as keyof (typeof providers)[typeof typedService]['eventsub'];
+
+    if (!service || !subscriptionType) {
+        return new NextResponse('Missing required fields', { status: 400 });
+    }
+
+    let generalSubscription = providers[typedService].eventsub[typedSubscriptionType];
+    if (!('event' in generalSubscription)) {
+        return new NextResponse('Invalid service or subscriptionType', { status: 400 });
+    }
+
+    let formatter: any = undefined;
+
+    try {
+        const mod = await import(`@/types/events/${generalSubscription.event}.ts`);
+        formatter = mod[generalSubscription.event];
+    } catch {
+        formatter = undefined;
+    }
+
+    const mockData = generateWithFormat(typedService, formatter);
+
+    return NextResponse.json({ service, subscription: subscriptionType, data: mockData });
 }
