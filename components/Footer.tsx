3ace38fd62c09a2beb8227f1b3faafed241dded1
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-11-19 13:36:23 -0600

    add icon link and reformat slightly

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,23 +5,33 @@ export default function Footer() {
         <footer id="footer">
             <div id="footer-box">
                 <span id="footer-info" className="footer-block">
-                    <h3>orbt</h3>
+                    <h3>
+                        <SVG name="star" />
+                        orbt
+                    </h3>
                     <p>
                         <span className="abn-sym">©</span> {new Date().getFullYear()} BradyDaLlama.
                     </p>
                 </span>
                 <span id="footer-links" className="footer-block">
-                    <a href="/discord">
-                        <SVG name="discord" tooltip={{ text: 'support discord' }} />
-                    </a>
-                    <a href="/shop">
-                        <SVG name="shop" tooltip={{ text: 'shop orbt widgets' }} />
-                    </a>
-                    <a href="/resources/docs">
-                        <SVG name="book" tooltip={{ text: 'developer docs' }} />
-                    </a>
-                    <a href="/legal/terms">terms</a>
-                    <a href="/legal/privacy">privacy policy</a>
+                    <span className="link-box">
+                        <a href="/discord">
+                            <SVG name="discord" tooltip={{ text: 'support discord' }} />
+                        </a>
+                        <a href="/shop">
+                            <SVG name="shop" tooltip={{ text: 'shop orbt widgets' }} />
+                        </a>
+                        <a href="/resources/docs">
+                            <SVG name="book" tooltip={{ text: 'developer docs' }} />
+                        </a>
+                        <a href="/resources/icons">
+                            <SVG name="star-4" tooltip={{ text: 'icons' }} />
+                        </a>
+                    </span>
+                    <span className="link-box">
+                        <a href="/legal/terms">terms</a>
+                        <a href="/legal/privacy">privacy policy</a>
+                    </span>
                 </span>
             </div>
         </footer>
