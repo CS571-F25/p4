196483050abbe0f30efea66a91de07e59a72be2b
BradyDaLlama <47823702+BradyDaLlama@users.noreply.github.com>
2025-12-13 10:01:40 -0600

    don't show log out button if the user is not logged in.

--- app/connections/page.tsx
+++ app/connections/page.tsx
@@ -1,10 +1,14 @@
 'use client';
 import '@/styles/providers.css';
 
+import { useUser } from '@/contexts/UserContext';
 import providers from '@/data/providers.json';
 import ProviderButton from '@/components/main/ProviderButton';
 
 export default function Connections() {
+    const { user } = useUser();
+    const orbtId = user?.orbtId;
+
     function logOut() {
         fetch('/api/user/cookie', {
             method: 'DELETE',
@@ -15,18 +19,20 @@ export default function Connections() {
 
     return (
         <div id="provider-container">
-            <p>include events & data from popular services</p>
+            <p>include events & data from popular services.</p>
             <div id="provider-button-container">
                 {Object.entries(providers).map(([provider, { color }], i) => (
                     <ProviderButton key={i} provider={provider} color={color} textStatus={true} />
                 ))}
             </div>
-            <span className="mt-2">
-                <button id="log-out" className="button" onClick={logOut}>
-                    log out
-                </button>
-                <p className="text-xxs">logging out does not delete connected services</p>
-            </span>
+            {orbtId && (
+                <span className="mt-2">
+                    <button id="log-out" className="button" onClick={logOut}>
+                        log out
+                    </button>
+                    <p className="text-xxs">logging out does not delete connected services</p>
+                </span>
+            )}
         </div>
     );
 }
